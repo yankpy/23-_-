c759caf60e7fedacc78cad047c02af007e11c25c
yankpy <819673953@qq.com>
2017-02-25 21:58:29 -0600

    Add files via upload
    登录界面类图及代码

---
+++ classProgram.java
@@ -0,0 +1,37 @@
+import java.sql.Connection;
+
+public class classProgram {
+	public class MainClass {
+		public void main(String args[]){
+			
+		}
+	}
+
+	public class LoginForm{
+		private UserDAO dao;
+		public void init(){
+			
+		}
+		public void display(){
+			
+		}
+		public void validate(){
+			
+		}
+	}
+
+	public class UserDAO{
+		private DBUtil db;
+		public boolean findUser(String userName,String userPassword){
+			return false;
+			
+		}
+	}
+
+	public class DBUtil{
+		public Connection getConnection(){
+			return null;
+			
+		}
+	}
+}
